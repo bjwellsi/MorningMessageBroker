769353b8a44abccd9e1284a21260eb7f1c447c4e
Braiden Wells <braiden@braidens-mbp.lan>
2024-06-05 18:12:01 -0500

    refactored a bit more

--- src/services/repositories/MessageRepo.js
+++ src/services/repositories/MessageRepo.js
@@ -1,18 +1,23 @@
-
 const fs = require("fs").promises;
 const MongoClient = require("mongodb").MongoClient;
 const MongoObjectID = require("mongodb").ObjectId;
 
-async function mongoConnection(collection) {
+async function getCollection() {
   var url = "mongodb://localhost:27017/morning_message_broker";
   let client = await MongoClient.connect(url);
-  return client.db().collection(collection);
+  return client.db().collection("messages");
+}
+
+async function getConnection() {
+  var url = "mongodb://localhost:27017/morning_message_broker";
+  let client = await MongoClient.connect(url);
+  return client.db().collection("messages");
 }
 
 async function getMessageList() {
   //currently just going to return all
   //should obviously be a bit more specific in the future, w/user for ex
-  const collection = await mongoConnection("messages");
+  let collection = await getCollection();
   return await collection.find().toArray();
 }
 
@@ -34,35 +39,34 @@ async function getNextRandomMessage(type) {
 }
 
 async function getMessageTypes() {
-  const collection = await mongoConnection("messages");
+  let collection = await getCollection();
   return await collection.distinct("type", {});
 }
 
 async function getMessagesByType(type) {
   //returns first message of a given type
-  const collection = await mongoConnection("messages");
   var query = { type: type };
+  let collection = await getCollection();
   return await collection.find(query).toArray();
 }
 
 async function getMessageByType(type) {
   //returns first message of a given type
-  const collection = await mongoConnection("messages");
   var query = { type: type };
-  return await collection.findOne(query);
+  let collection = await getCollection();
+  return await collection().findOne(query);
 }
 
 async function getMessageByID(id) {
-  const collection = await mongoConnection("messages");
   oid = MongoObjectID.createFromHexString(id);
   var query = { _id: oid };
+  let collection = await getCollection();
   return await collection.findOne(query);
 }
 
 async function insertMessage(message) {
   //basically validate the json
   //obviously not a pretty way to do it
-  const collection = mongoConnection("messages");
   if (typeof message.destination != "string" || message.destination === "")
     throw new Error("Invalid Destination");
 
@@ -72,18 +76,18 @@ async function insertMessage(message) {
   if (typeof message.data != "string" || message.data === "")
     throw new Error("Invalid Data");
 
+  let collection = await getCollection();
   return await collection.insertOne(message);
 }
 
 async function deleteMessageByID(id) {
-  const collection = await mongoConnection("messages");
   const oid = MongoObjectID.createFromHexString(id);
   const query = { _id: oid };
+  let collection = await getCollection();
   return await collection.deleteOne(query);
 }
 
 module.exports = {
-  mongoConnection,
   insertMessage,
   getMessageByType,
   getNextRandomMessage,
